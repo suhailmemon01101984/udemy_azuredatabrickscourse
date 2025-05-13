bd1f47b55941d8e182739cc0aa837b61c31f1571
suhailmemon01101984 <suhailmemon01101984>
2025-05-13 00:44:32 +0000

    01-Notebook-Introduction-Solution.py

---
+++ 01-Notebook-Introduction-Solution.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC - <a href="https://docs.databricks.com/en/notebooks/index.html">**Notebooks Documentation**</a>
+# MAGIC
+# MAGIC Notebook Name : 01-Notebook-Introduction
+# MAGIC
+# MAGIC | Magic Command | Language Selection |
+# MAGIC | --- | --- |
+# MAGIC | %md  | Markdown language |
+# MAGIC | %py  | Python language |
+# MAGIC | %sql | SQL |
+# MAGIC | %scala | Scala or Java Language |
+# MAGIC | %R | R Programming Language |
