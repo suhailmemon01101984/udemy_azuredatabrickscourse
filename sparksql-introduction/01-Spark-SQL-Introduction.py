87bb282affbe8dae2fae4d7a46e6e0ac0e884945
suhailmemon01101984 <suhailmemon01101984>
2025-08-27 01:34:16 +0000

    01-spark-sql-introduction.py

--- sparksql-introduction/01-Spark-SQL-Introduction.py
+++ sparksql-introduction/01-Spark-SQL-Introduction.py
@@ -2,11 +2,11 @@
 # MAGIC %md
 # MAGIC ##### Data File Path in DataLake Storage Account
 # MAGIC
-# MAGIC CSV Source File Path : "abfss://working-labs@datalakestorageaccountname.dfs.core.windows.net/bronze/daily-pricing/csv"
+# MAGIC CSV Source File Path : "abfss://working-labs-dev@dbrkcrse20251storagedev.dfs.core.windows.net/bronze/daily-pricing/csv"
 # MAGIC
-# MAGIC JSON Source  File Path : "abfss://working-labs@datalakestorageaccountname.dfs.core.windows.net/bronze/daily-pricing/json"
+# MAGIC JSON Source  File Path : "abfss://working-labs-dev@dbrkcrse20251storagedev.dfs.core.windows.net/bronze/daily-pricing/json"
 # MAGIC
-# MAGIC PARQUET Source  File Path : "abfss://working-labs@datalakestorageaccountname.dfs.core.windows.net/bronze/daily-pricing/parquet"
+# MAGIC PARQUET Source  File Path : "abfss://working-labs-dev@dbrkcrse20251storagedev.dfs.core.windows.net/bronze/daily-pricing/parquet"
 # MAGIC
 # MAGIC
 # MAGIC ###### Spark Session Methods
@@ -14,3 +14,46 @@
 # MAGIC
 # MAGIC ###### Dataframes To/From SQL Conversions
 # MAGIC - <a href="https://spark.apache.org/docs/3.1.3/api/python/reference/api/pyspark.sql.DataFrame.createOrReplaceTempView.html" target="_blank">DataFrame-SQLConversions</a> :**`createOrReplaceTempView`** ,**`spark.sql`**  ,**`createOrReplaceGlobalTempView`**
+
+# COMMAND ----------
+
+storageAccountKey=''
+spark.conf.set("fs.azure.account.key.dbrkcrse20251storagedev.dfs.core.windows.net",storageAccountKey)
+
+
+# COMMAND ----------
+
+sourceCSVFilePath='abfss://working-labs-dev@dbrkcrse20251storagedev.dfs.core.windows.net/bronze-dev/daily-pricing/csv'
+sourceJSONFilePath='abfss://working-labs-dev@dbrkcrse20251storagedev.dfs.core.windows.net/bronze-dev/daily-pricing/json'
+sourceParquetFilePath='abfss://working-labs-dev@dbrkcrse20251storagedev.dfs.core.windows.net/bronze-dev/daily-pricing/parquet'
+
+# COMMAND ----------
+
+sourceCSVFileDF=spark.read.option("header","true").csv(sourceCSVFilePath)
+
+# COMMAND ----------
+
+sourceCSVFileDF.createOrReplaceTempView("daily_pricing_temp_vw")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from daily_pricing_temp_vw
+
+# COMMAND ----------
+
+spark.sql("select * from daily_pricing_temp_vw")
+
+# COMMAND ----------
+
+spark.table("daily_pricing_temp_vw")
+
+# COMMAND ----------
+
+sourceCSVFileDF.createOrReplaceGlobalTempView("daily_pricing_global_temp_vw")
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from global_temp.daily_pricing_global_temp_vw
