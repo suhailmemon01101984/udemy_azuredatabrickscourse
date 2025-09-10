14ea7871ac5338298676d6a90607f6efa9f153a3
suhailmemon01101984 <suhailmemon01101984>
2025-09-10 02:37:01 +0000

    sparksql-introduction/04-Spark-SQL-Transformations.sql

---
+++ sparksql-introduction/04-Spark-SQL-Transformations.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ##### Source File Details
+-- MAGIC CSV Source File Path : "abfss://working-labs@datalakestorageaccountname.dfs.core.windows.net/bronze-dev/daily-pricing/csv"
+-- MAGIC
+-- MAGIC
+-- MAGIC ###### Spark Session Methods
+-- MAGIC - <a href="https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.sql.SparkSession.html" target="_blank">SparkSessionMethods</a> :**`read`**,**`write`**,  **`sql`** ,  **`table`** ,  **`createDataFrame`**
+-- MAGIC
+-- MAGIC ###### SQL Transformations
+-- MAGIC - <a href="https://spark.apache.org/docs/3.1.3/api/python/reference/api/pyspark.sql.DataFrame.createOrReplaceTempView.html" target="_blank">SQLFunctions</a> :**`select`** ,**`distinct`**  ,**`where`** ,**`order by`**
